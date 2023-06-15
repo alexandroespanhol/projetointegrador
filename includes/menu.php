dffa01a60141bcb0826538132ea143b0cb218545
alexandro.r.c.espanhol <112526166+alexandroespanhol@users.noreply.github.com>
2023-06-15 19:04:27 -0300

    projeto
    projeto integrador

---
+++ includes/menu.php
@@ -0,0 +1,5 @@
+<nav>
+    <a href="#">Ongs-doadores</a>
+    <a href="#">pets</a>
+    <a href="../painel/logout.php">Sair</a>
+</nav>
---
+++ includes/rodape.php
@@ -0,0 +1,6 @@
+<footer>
+    <p style="text-align:center;">
+    &copy; Todos os direitos reservados.Crido por <a href="mailto:alexandro.spanhol@gmail.com">alexandro Espanhol</a>
+</p>
+</footer>
+
